494c358da64451db587ebaa4f020af2063addebb
EC2 Default User <ec2-user@ip-172-31-43-22.ap-northeast-1.compute.internal>
2022-08-28 17:06:40 +0000

    finish[model_answer]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,30 +7,30 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
-  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :following_user, through: :follower, source: :followed
-  has_many :follower_user, through: :followed, source: :follower
-  
-  def follow(user_id)
-    follower.create(followed_id: user_id)
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+
+  def follow(user)
+    relationships.create(followed_id: user.id)
   end
-  
-  def unfollow(user_id)
-    follower.find_by(followed_id: user_id).destroy
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
   end
-  
+
   def following?(user)
-    following_user.include?(user)
+    followings.include?(user)
   end
-  
-  
+
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
