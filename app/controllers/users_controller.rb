a5d91b5a66ada9944ba5b081d5a4d45655426c8a
EC2 Default User <ec2-user@ip-172-31-43-22.ap-northeast-1.compute.internal>
2022-08-28 15:50:44 +0000

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
 
   def index
     @users = User.all
     @book = Book.new
+    @following_users = current_user.following_user
+    @follower_users = current_user.follower_user
   end
 
   def edit
@@ -27,6 +31,16 @@ class UsersController < ApplicationController
     else
       render "edit"
     end
+  end
+
+  def follows
+    user = User.find(params[:id])
+    @users = user.following_user
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.follower_user
   end
 
   private
